04bb9f64f6bc6693a8b352a7cf44eca3549b80ca
Abhishek-Verma75230 <itsabhishek75230@gmail.com>
2024-03-15 17:13:14 +0530

    navbar is added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,16 @@ import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import YearSelection from './components/YearSelection';
 import Branch from './components/Branch';
 import Upload from './components/Upload';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    
     <Router>
+      <Navbar />
+      
       <Routes>
+     
       <Route path="/abhishek@123" element={<Upload />} />
         <Route path="/" element={<YearSelection />} />
         <Route path="/branches/:year" element={<Branch/>} />
