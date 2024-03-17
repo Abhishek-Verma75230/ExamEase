db5db92ed5370a4fb5590b7847499763c232d48e
Ashim Banerjee <125278221+210310624729@users.noreply.github.com>
2024-03-17 17:06:20 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-import YearSelection from './components/YearSelection';
-import Branch from './components/Branch';
-import Upload from './components/Upload';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Branches from "./components/Branches";
+import Navbar from "./components/Navbar";
+
+
+
 
 function App() {
+  
   return (
-    
     <Router>
-      <Navbar />
       
+      <Navbar />
+
       <Routes>
-     
-      <Route path="/abhishek@123" element={<Upload />} />
-        <Route path="/" element={<YearSelection />} />
-        <Route path="/branches/:year" element={<Branch/>} />
+        {/* <Route path="/abhishek@123" element={<Upload />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/branches/:year" element={<Branches />} />
       </Routes>
+      
     </Router>
   );
 }
