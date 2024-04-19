6bc2a8f54659e983b042234e45b478bf1470146a
Gargi Jaiswal <134698352+Gargi-jais11@users.noreply.github.com>
2024-04-20 02:22:26 +0530

    footer UI

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -381,17 +381,37 @@ const Home = () => {
         </div>
         <div className="bg-light">
                 <div className="row">
-    <div className="col-md-2">
-        <p className="text-left fs-3 my-3 fw-bold border-end border-dark hide-on-mobile" style={{ color: 'black', fontFamily: "Poppins, sans-serif" }}>ExamEase</p>
+    <div className="col-md-3">
+        <p className=" text-left fs-3 my-3 fw-bold border-end border-dark hide-on-mobile" style={{ color: 'black', fontFamily: "Poppins, sans-serif" }}>ExamEase</p>
     </div>
-    <div className="col-md-2">
-        <p className="text-left my-3 " style={{ color: '#111827', fontFamily: "Poppins, sans-serif",fontSize:"18px" }}>Copyright 
+    <div className="col-md-3">
+        <p className=" text-left my-3 " style={{ color: '#111827', fontFamily: "Poppins, sans-serif",fontSize:"18px" }}>Copyright 
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="blackr" className="bi bi-c-circle" viewBox="0 0 16 16">
                 <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512"/>
             </svg>ExamEase.com
             
         </p>
     </div>
+    <div class="col-md-3 text-center my-3 d-flex offset-md-3 " style={{fontSize:"18px",color:"black"}}>
+        
+          Follow us on: 
+          
+          <a href="https://www.linkedin.com/in/gargi-jaiswal-455617246" target="_blank"style={{marginRight: "10px",color:"black",marginLeft:"10px"}}>
+            <i class="fab fa-linkedin" style={{fontSize: "25px", color:"blue"}}></i>
+          </a>
+          <a href="https://github.com/Gargi-jais11" target="_blank" style={{marginRight: "10px"}}>
+            <i class="fab fa-github" style={{fontSize: "25px"}}></i>
+          </a>
+          <a href="https://instagram.com/gargi_jaisss?igshid=MzNlNGNkZWQ4Mg==" target="_blank" style={{marginRight: "10px"}}>
+            <i class="fab fa-instagram" style={{fontSize: "25px",color:"magenta"}}></i>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100030824889804&mibextid=ZbWKwL" target="_blank" style={{marginRight: "10px"}}>
+            <i class="fab fa-facebook" style={{fontSize: "25px",color:"blue"}}></i>
+          </a>
+          
+      
+        
+      </div>
 </div>
 </div>
       </div>
