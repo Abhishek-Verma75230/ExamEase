106e51963f9967f328517f119d8b22d98098cf31
Abhishek-Verma75230 <itsabhishek75230@gmail.com>
2024-11-18 19:13:31 +0530

    new changes

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,13 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-    const Connection=async ()=>{
-    const URL=`mongodb://education:education123@ac-ocravo9-shard-00-00.mxhzlac.mongodb.net:27017,ac-ocravo9-shard-00-01.mxhzlac.mongodb.net:27017,ac-ocravo9-shard-00-02.mxhzlac.mongodb.net:27017/?ssl=true&replicaSet=atlas-s1yls6-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
-    try {
-       await mongoose.connect(URL,{useNewUrlParser:true});
-       console.log("database connected successfully to server ..");
-    } catch (error) {
-        console.log("error while connecting database");
-    }
-}
+dotenv.config(); 
+
+const Connection = async () => {
+  const PASSWORD = process.env.MONGODB_PASSWORD; 
+  const URL = `mongodb://education:${PASSWORD}@ac-ocravo9-shard-00-00.mxhzlac.mongodb.net:27017,ac-ocravo9-shard-00-01.mxhzlac.mongodb.net:27017,ac-ocravo9-shard-00-02.mxhzlac.mongodb.net:27017/?ssl=true&replicaSet=atlas-s1yls6-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
+
+  try {
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected successfully to server.');
+  } catch (error) {
+    console.error('Error while connecting to database:', error.message);
+  }
+};
 
 export default Connection;
